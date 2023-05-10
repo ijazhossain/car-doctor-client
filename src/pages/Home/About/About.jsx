ce8fb98b2a1b21a34c04a7e785977d4f9ce199a3
ijaz35 <ijaz.hossain1@gmail.com>
2023-05-10 18:17:04 +0600

    home about section added

---
+++ src/pages/Home/About/About.jsx
@@ -0,0 +1,26 @@
+import parts from '../../../assets/images/about_us/parts.jpg'
+import person from '../../../assets/images/about_us/person.jpg'
+
+const About = () => {
+    return (
+        <div className="hero  bg-base-200 my-12 py-16 px-8">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className='lg:w-1/2 relative'>
+                    <img
+                        src={person} className="w-3/4 rounded-lg shadow-2xl" />
+                    <img
+                        src={parts} className='w-1/2 absolute right-6 top-1/2 border-8 rounded-lg' alt="" />
+
+                </div>
+                <div className='lg:w-1/2 space-y-5'>
+                    <h2 className='text-3xl text-orange-400 font-semibold'>About Us</h2>
+                    <h1 className='text-5xl font-semibold'>We are qualified & of experience in this field</h1>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -26,6 +26,15 @@ const Banner = () => {
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img src={img2} className="w-full" />
+                <div className="absolute top-0 pl-12 flex flex-col items-start justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full text-white">
+                    <h1 className="text-6xl ">Affordable Price For Car Servicing</h1>
+                    <p className='my-8'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                    <div>
+                        <button className="btn btn-error me-5">Discover More</button>
+                        <button className="btn btn-outline btn-accent">Latest Projects</button>
+                    </div>
+
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
@@ -33,6 +42,15 @@ const Banner = () => {
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img src={img3} className="w-full" />
+                <div className="absolute top-0 pl-12 flex flex-col items-start justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full text-white">
+                    <h1 className="text-6xl ">Affordable Price For Car Servicing</h1>
+                    <p className='my-8'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                    <div>
+                        <button className="btn btn-error me-5">Discover More</button>
+                        <button className="btn btn-outline btn-accent">Latest Projects</button>
+                    </div>
+
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
@@ -40,6 +58,15 @@ const Banner = () => {
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <img src={img4} className="w-full" />
+                <div className="absolute top-0 pl-12 flex flex-col items-start justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full text-white">
+                    <h1 className="text-6xl ">Affordable Price For Car Servicing</h1>
+                    <p className='my-8'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                    <div>
+                        <button className="btn btn-error me-5">Discover More</button>
+                        <button className="btn btn-outline btn-accent">Latest Projects</button>
+                    </div>
+
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide5" className="btn btn-circle">❯</a>
@@ -47,6 +74,15 @@ const Banner = () => {
             </div>
             <div id="slide5" className="carousel-item relative w-full">
                 <img src={img5} className="w-full" />
+                <div className="absolute top-0 pl-12 flex flex-col items-start justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full text-white">
+                    <h1 className="text-6xl ">Affordable Price For Car Servicing</h1>
+                    <p className='my-8'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                    <div>
+                        <button className="btn btn-error me-5">Discover More</button>
+                        <button className="btn btn-outline btn-accent">Latest Projects</button>
+                    </div>
+
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide6" className="btn btn-circle">❯</a>
@@ -54,6 +90,15 @@ const Banner = () => {
             </div>
             <div id="slide6" className="carousel-item relative w-full">
                 <img src={img6} className="w-full" />
+                <div className="absolute top-0 pl-12 flex flex-col items-start justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full h-full text-white">
+                    <h1 className="text-6xl ">Affordable Price For Car Servicing</h1>
+                    <p className='my-8'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                    <div>
+                        <button className="btn btn-error me-5">Discover More</button>
+                        <button className="btn btn-outline btn-accent">Latest Projects</button>
+                    </div>
+
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide5" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
